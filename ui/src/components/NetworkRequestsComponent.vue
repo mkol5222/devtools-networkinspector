0b3e10073e3e861b949e60e6e6cf8f717d566f93
Martin Koldodovsky <mkoldov@gmail.com>
2023-11-07 15:28:01 +0100

    quasar ui

---
+++ ui/src/components/NetworkRequestsComponent.vue
@@ -0,0 +1,69 @@
+<template>
+
+    <q-card class="my-card bg-secondary text-white full-width">
+      <q-card-section>
+        <q-table
+          title="Network Requests"
+          :rows="requests"
+          :columns="columns"
+          row-key="name"
+        />
+      </q-card-section>
+    </q-card>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NetworkRequestsComponent',
+  props: {},
+  data: () => ({
+    requests: [
+      {
+        url: 'https://www.google.com',
+        method: 'GET',
+        status: 200,
+        statusText: 'OK!',
+      },
+      {
+        url: 'https://www.dev.to',
+        method: 'POST',
+        status: 200,
+        statusText: 'OK',
+      },
+    ],
+    columns: [
+      {
+        name: 'method',
+        align: 'left',
+        label: 'Method',
+        field: 'method',
+        sortable: true,
+      },
+      {
+        name: 'url',
+        align: 'left',
+        label: 'URL',
+        field: 'url',
+        sortable: true,
+      },
+      {
+        name: 'status',
+        align: 'left',
+        label: 'Status',
+        field: 'status',
+        sortable: true,
+      },
+      {
+        name: 'statusText',
+        align: 'left',
+        label: 'Status text',
+        field: 'statusText',
+        sortable: true,
+      },
+    ],
+  }),
+});
+</script>
---
+++ ui/src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <NetworkRequestsComponent></NetworkRequestsComponent>
+  </q-page>
+</template>
+
+<script lang="ts">
+
+import NetworkRequestsComponent from 'components/NetworkRequestsComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { NetworkRequestsComponent },
+  setup () {
+
+    return {  };
+  }
+});
+</script>
